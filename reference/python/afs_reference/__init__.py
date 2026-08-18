00b49929253794722e8b5c0c15444a46a13c8481
Stefan Wanner <swanner4096+account@gmail.com>
2026-08-18 13:20:16 +0200

    ref(reference): export PackML runtime

--- reference/python/afs_reference/__init__.py
+++ reference/python/afs_reference/__init__.py
@@ -1,6 +1,15 @@
 """Executable AFS reference implementation."""
 
 from .application import Application
+from .composition import CompositeUnit
+from .packml import PackMLCommand, PackMLLifecycle, PackMLState
 from .unit import AFS_TEMPLATE_01_UNIT
 
-__all__ = ["AFS_TEMPLATE_01_UNIT", "Application"]
+__all__ = [
+    "AFS_TEMPLATE_01_UNIT",
+    "Application",
+    "CompositeUnit",
+    "PackMLCommand",
+    "PackMLLifecycle",
+    "PackMLState",
+]
